63168248b40f0d938c6b9d04adefa136106490bd
admin-feng-9527 <lumingfei9527@163.com>
2020-06-07 15:20:39 +0800

    zuul路由网关

---
+++ microservicecloud-zuul-geteway-9527/src/main/java/com/springcloud/Zuul_App.java
@@ -0,0 +1,9 @@
+package com.springcloud;
+
+
+@SpringBootApplication
+public class Zuul_App {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_App.class,args);
+    }
+}
