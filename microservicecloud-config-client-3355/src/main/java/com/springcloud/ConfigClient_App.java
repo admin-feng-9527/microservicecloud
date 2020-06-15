f22b48fe4cc31163560a3c1bff330ccc6ddbda14
admin-feng-9527 <lumingfei9527@163.com>
2020-06-15 11:12:50 +0800

    config集中配置

---
+++ microservicecloud-config-client-3355/src/main/java/com/springcloud/ConfigClient_App.java
@@ -0,0 +1,16 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author root
+ * @createTime 31 19:14
+ * @description
+ */
+@SpringBootApplication
+public class ConfigClient_App {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_App.class, args);
+    }
+}
